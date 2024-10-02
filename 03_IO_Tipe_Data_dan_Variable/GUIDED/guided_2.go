497a154486c2fd8d15bf69bbc6ffc64d97f026d0
Khanif Yunan Pratama <124347280+Khananta@users.noreply.github.com>
2024-10-02 09:04:35 +0700

    Update guided_2.go

--- 03_IO_Tipe_Data_dan_Variable/GUIDED/guided_2.go
+++ 03_IO_Tipe_Data_dan_Variable/GUIDED/guided_2.go
@@ -17,7 +17,7 @@ func main() {
 	fmt.Print("Masukan tinggi alas segitiga: ")
 	fmt.Scan(&tinggi)
 
-	hasil = (alas * tinggi) * 1 / 2
+	hasil = (alas * tinggi) * 1 / 2 //Rumus untuk menentukan luas segitiga
 	fmt.Println("Luas segitiga adalah: ", hasil)
 
 }
