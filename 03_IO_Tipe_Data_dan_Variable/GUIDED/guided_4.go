5cbddd8f2ac49a20f4481c181676ea3d7b601535
Khanif Yunan Pratama <124347280+Khananta@users.noreply.github.com>
2024-10-02 09:05:24 +0700

    Update guided_4.go

--- 03_IO_Tipe_Data_dan_Variable/GUIDED/guided_4.go
+++ 03_IO_Tipe_Data_dan_Variable/GUIDED/guided_4.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Print("Masukan nilai Rupiah: ")
 	fmt.Scan(&rupiah)
 
-	dolar = rupiah / 15000
+	dolar = rupiah / 15000 //Rumus untuk konfersi rupiah ke dolar
 	fmt.Println("Konfersi Rupiah ke Dolar adalah: ", dolar)
 
 }
