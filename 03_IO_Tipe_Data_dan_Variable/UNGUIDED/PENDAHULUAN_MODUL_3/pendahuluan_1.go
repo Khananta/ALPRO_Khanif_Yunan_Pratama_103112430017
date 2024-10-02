c1a6152fe0f5cbb446b4ef5fd1d170667a4f6190
Khanif Yunan Pratama <124347280+Khananta@users.noreply.github.com>
2024-10-02 15:13:17 +0700

    Update pendahuluan_1.go

--- 03_IO_Tipe_Data_dan_Variable/UNGUIDED/PENDAHULUAN_MODUL_3/pendahuluan_1.go
+++ 03_IO_Tipe_Data_dan_Variable/UNGUIDED/PENDAHULUAN_MODUL_3/pendahuluan_1.go
@@ -12,7 +12,7 @@ func main() {
 	keliling := 4 * sisi //Rumus yang digunakan untuk menemukan keliling persegi
 	luas := sisi * sisi  //Rumus yang digunakan untuk menghitung luas dari sebuah persegi
 
-	// Output yang digunakan untuk menampilkan sebuah hasil perhitungan keliling dan persegi.
+	// Output yang digunakan untuk menampilkan sebuah hasil perhitungan keliling dan luas persegi.
 	fmt.Print("Keliling dari sebuah persegi adalah: ", keliling, " Meter")
 	fmt.Print(" dan luas dari sebuah persegi adalah: ", luas, " Meter Persegi")
 }
