60bf173f8c8c56035145cb98b72a9677ba73b1db
Khanif Yunan Pratama <124347280+Khananta@users.noreply.github.com>
2024-10-02 14:59:19 +0700

    Add files via upload

---
+++ 03_IO_Tipe_Data_dan_Variable/UNGUIDED/PENDAHULUAN_MODUL_3/pendahuluan_3.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		suhuFahrenheit float32
+		suhuKelvin     float32
+	)
+
+	// Inputan untuk memasukan suhu dalam fahrenheit.
+	fmt.Print("Masukan suhu dalam Fahrenheit: ")
+	fmt.Scanln(&suhuFahrenheit)
+
+	//Operasi untuk mengkonfersi dari fahrenheit ke suhu kelvin
+	suhuKelvin = (suhuFahrenheit-32)*5/9 + 273.15
+	fmt.Println("Suhu Kelvin nya adalah: ", suhuKelvin, "Kelvin")
+
+}
