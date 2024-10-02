ac2e22a07028983fb3c95d375df18c7b3b6381e1
Khanif Yunan Pratama <124347280+Khananta@users.noreply.github.com>
2024-10-02 08:38:33 +0700

    Update latso_empat.go

--- 02_Tipe_Data_dan_Variable/UNGUIDED/latso_empat.go
+++ 02_Tipe_Data_dan_Variable/UNGUIDED/latso_empat.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Print("Masukan suhu dalam Fahrenheit: ")
 	fmt.Scanln(&suhuFahrenheit)
 
-	suhuCelcius = (suhuFahrenheit - 32) * 5 / 9
+	suhuCelcius = (suhuFahrenheit - 32) * 5 / 9 //Rumus untuk konfersi suhu fahrenheit ke celcius 
 	fmt.Println("Suhu Celcius nya adalah: ", suhuCelcius)
 
 }
